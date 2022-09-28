f1a2f9c3483f3876d50bdbba8e3ca46eb7dbe79a
André L. Bandeli <87938869+andre-bandeli@users.noreply.github.com>
2022-09-28 04:00:59 -0300

    Delete Netflix directory

--- Netflix/page/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-   
-# creating a form 
-class InputForm(forms.Form):
-   
-    username = forms.CharField(max_length = 200)
-    password = forms.CharField(widget = forms.PasswordInput())
